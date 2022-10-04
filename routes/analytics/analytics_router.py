bfd75e7e3204eee10d3ab29f496ddc6cf0fdae0c
Anuran Roy <76481787+anuran-roy@users.noreply.github.com>
2022-10-05 02:01:33 +0530

    Added value aggregation mechanism

--- routes/analytics/analytics_router.py
+++ routes/analytics/analytics_router.py
@@ -10,8 +10,9 @@ router = APIRouter(prefix="/analytics", tags=["Analytics API"])
 
 @router.get("/scanhistory")
 async def scan_history_analytics(db: TinyDB = Depends(get_db)):
-    all_timestamps = [dict(i)["timestamp"] for i in db.all()]
-    all_counts = [sum([i["report"][k]["count"] for k in i["report"].keys()]) for i in db.all()]
+    scan_table = db.table("scans")
+    all_timestamps = [dict(i)["timestamp"] for i in scan_table.all()]
+    all_counts = [sum([i["report"][k]["count"] for k in i["report"].keys()]) for i in scan_table.all()]
     # choices = [np.random.randint(5, (2,5)) for _ in range(10)]
     print(all_timestamps, all_counts)
     time.sleep(2)
