a098afc3a675ab60b6be6a0c57191dbaf7f1e2f9
Anuran Roy <76481787+anuran-roy@users.noreply.github.com>
2022-10-05 05:26:36 +0530

    Added data transfer log DB

--- db/db.py
+++ db/db.py
@@ -3,10 +3,19 @@ from main import BASE_DIR
 import os
 
 db_loc = str(BASE_DIR / "db.json")
-db = TinyDB(db_loc)
+log_db_loc = str(BASE_DIR / "sync_data" / "log_db.json")
+
 
 if not os.path.exists(db_loc):
-    os.system(f"touch {db_loc}")
+    open(db_loc, "w").close()
+
+
+if not os.path.exists(log_db_loc):
+    os.makedirs(str(BASE_DIR / "sync_data"), exist_ok=True)
+    open(log_db_loc, "w").close()
+
+db = TinyDB(db_loc)
+log_db = TinyDB(log_db_loc)
 
 
 def get_db():
@@ -15,3 +24,11 @@ def get_db():
         yield db
     finally:
         print("Database access complete.")
+
+
+def get_log_db():
+    try:
+        print("Accessing Log DB...")
+        yield log_db
+    finally:
+        print("Log Database access complete.")
