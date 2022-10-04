373eb4e0901f1def961d0e2e3929b9cb74caa31f
Anuran Roy <76481787+anuran-roy@users.noreply.github.com>
2022-10-05 01:20:24 +0530

    Created credentials router, added routes for creating and listing credentials, either all or selectively based on email, phone, or username

---
+++ routes/credentials/credentials_router.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from typing import Any, Optional, List, Dict
+from db.db import get_db
+from models.models import CredentialModel
+import uuid
+from tinydb import TinyDB, Query
+
+router = APIRouter(prefix="/credentials", tags=["Credentials Scanner"])
+
+
+@router.post("/add", status_code=status.HTTP_201_CREATED)
+async def add_credential(
+    credential: CredentialModel,
+    db: TinyDB = Depends(get_db)
+):
+    cred_table = db.table("credentials")
+
+    credQuery = Query()
+    queryset = cred_table.search(
+        (credQuery.credential == credential.credential) &
+        (credQuery.credential_type == credential.credential_type)
+    )
+
+    if len(queryset) > 0:
+        return HTTPException(
+            status.HTTP_406_NOT_ACCEPTABLE,
+            detail="Entry already exists!"
+        )
+
+    cred = dict({**dict(credential), "id": str(uuid.uuid4())})
+
+    cred_table.insert(cred)
+    return {"status": status.HTTP_201_CREATED, "created_credential": cred}
+
+
+@router.get("/", response_model=List[Dict[str, Any]])
+async def get_credentials(
+    credType: Optional[str] = "all",
+    db: TinyDB = Depends(get_db)
+):
+    cred_table = db.table("credentials")
+    if credType == "all":
+        return cred_table.all()
+    else:
+        return cred_table.search(Query().credential_type == credType)
