4eaeebade8a7d2842f490a3858e00f105431dc1d
Anuran Roy <76481787+anuran-roy@users.noreply.github.com>
2022-10-04 15:07:00 +0530

    Added Database and other dependencies

--- db/db.py
+++ db/db.py
@@ -1,7 +1,12 @@
 from tinydb import TinyDB
 from main import BASE_DIR
+import os
 
-db = TinyDB(str(BASE_DIR / "db.json"))
+db_loc = str(BASE_DIR / "db.json")
+db = TinyDB(db_loc)
+
+if not os.path.exists(db_loc):
+    os.system(f"touch {db_loc}")
 
 
 def get_db():
