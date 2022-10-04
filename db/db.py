814592e7c8603838c34b9c0659d69fb701372406
Anuran Roy <76481787+anuran-roy@users.noreply.github.com>
2022-10-04 14:58:57 +0530

    Added Database and other dependencies

--- db/db.py
+++ db/db.py
@@ -1,2 +1,12 @@
+from tinydb import TinyDB
+from main import BASE_DIR
+
+db = TinyDB(str(BASE_DIR / "db.json"))
+
+
 def get_db():
-    return {}
+    try:
+        print("Accessing DB...")
+        yield db
+    finally:
+        print("Database access complete.")
